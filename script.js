d59109c07a2cdf7c186cb957ee199ff900d55113
Courtnee McCollum <courtneemeaghan@gmail.com>
2022-04-25 05:02:13 -0500

    Refactor gameboard array and add display for user to click

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 const Gameboard = {
-  gameboard: [
-               ['', '', ''],
-               ['', '', ''],
-               ['', '', '']
+  gameboard: ['', '', '',
+              '', '', '',
+              '', '', ''
              ],
+  updateGameboard: function(marker, i) {
+    this.gameboard[i] = marker;
+  },
   winningCombos: [
-                   [ [[0], [0]], [[0], [1]], [[0], [2]] ],
-                   [ [[1], [0]], [[1], [1]], [[1], [2]] ],
-                   [ [[2], [0]], [[2], [1]], [[2], [2]] ],
-                   [ [[0], [0]], [[1], [0]], [[2], [0]] ],
-                   [ [[0], [1]], [[1], [1]], [[2], [1]] ],
-                   [ [[0], [2]], [[1], [2]], [[2], [2]] ],
-                   [ [[0], [0]], [[1], [1]], [[2], [2]] ],
-                   [ [[0], [2]], [[1], [1]], [[2], [0]] ]
+                   [0, 1, 2],
+                   [3, 4, 5],
+                   [6, 7, 8],
+                   [0, 3, 6],
+                   [1, 4, 7],
+                   [2, 5, 8],
+                   [0, 4, 8],
+                   [2, 4, 6]
                  ]
+}
+
+function Player(name, marker) {
+  return {name, marker};
 }
 
 const Gameplay = {
@@ -23,26 +29,25 @@ const Gameplay = {
 
   startRound: function() {
     if (this.currentPlayer === null) this.currentPlayer = this.player1;
-  },
-  endTurn: function() {
+    
     this.checkForWin(this.currentPlayer);
     this.checkForGameOver();
-
-    this. currentPlayer = this.currentPlayer === this.player1 ? this.player2 : this.player1;
+  },
+  endRound: function() {
+    this.currentPlayer = this.currentPlayer === this.player1 ? this.player2 : this.player1;
   },
   checkForWin: function(currentPlayer) {
     const gameWon = Gameboard.winningCombos.some(combos => {
-      return combos.every(([i, j]) => {
-        return Gameboard.gameboard[i][j] === currentPlayer.marker;
+      return combos.every(i => {
+        console.log(Gameboard.gameboard[i] === currentPlayer.marker);
+        return Gameboard.gameboard[i] === currentPlayer.marker;
       });
     });
 
     if (gameWon) this.gameWon(currentPlayer);
   },
   checkForGameOver: function() {
-    let fullGameboard = Gameboard.gameboard.every(row => {
-      return row.every(square => square);
-    });
+    let fullGameboard = Gameboard.gameboard.every(square => square);
 
     if (fullGameboard) this.endGame();
   },
@@ -54,8 +59,30 @@ const Gameplay = {
   }
 }
 
-Gameplay.startRound();
+const EventHandler = {
+  gameboard: function() {
+    const gameboardSquares = [...document.querySelectorAll('.gameboard-square')];
+    gameboardSquares.forEach(function(square) {
+      square.addEventListener('click', function(e) {
+        const i = [...document.querySelectorAll('.gameboard-square')].indexOf(e.target);
 
-function Player(name, marker) {
-  return {name, marker};
+        Gameplay.startRound();
+        DisplayController.updateGameboard(i);
+        Gameboard.updateGameboard(Gameplay.currentPlayer.marker, i);
+        Gameplay.endRound();
+      });
+    });
+  }
 }
+
+const DisplayController = {
+  updateGameboard: function(i) {
+    const gameboardSquares = [...document.querySelectorAll('.gameboard-square')];
+    const squareP = document.createElement('p');
+    
+    squareP.textContent = Gameplay.currentPlayer.marker;
+    gameboardSquares[i].appendChild(squareP);
+  }
+}
+
+EventHandler.gameboard();
